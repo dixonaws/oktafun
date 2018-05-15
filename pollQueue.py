a3552a0464d782e1aa39770eed81fa4d429efd6c
dixonaws <dixonaws@amazon.com>
2018-05-15 14:44:01 -0400

    implemented short polling with 10 sec timeout, 30 sec visibility timeout

--- pollQueue.py
+++ pollQueue.py
@@ -4,10 +4,10 @@ clientSqs=boto3.client('sqs')
 
 # more info at http://boto3.readthedocs.io/en/latest/reference/services/sqs.html#SQS.Client.receive_message
 # returns a dict
-dictMessages=clientSqs.receiveMessage(
-    queryUrl='',
-    AttributeNames='All',
-    MessageAttributeNames='All',
+dictMessages=clientSqs.receive_message(
+    QueueUrl='https://sqs.us-east-1.amazonaws.com/477157386854/oktafun',
+    AttributeNames=['All'],
+    MessageAttributeNames=['All'],
     MaxNumberOfMessages=10,
     VisibilityTimeout=30,
     WaitTimeSeconds=10,
